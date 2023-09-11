7fd562265a50ed1fe356bab501632a406346ccfd
mimaraka <nj.ot.niconico@gmail.com>
2023-09-11 14:41:10 +0900

    Update mashilog.py

--- modules/mashilog.py
+++ modules/mashilog.py
@@ -5,12 +5,12 @@ LogType = Literal["info", "error"]
 
 def mashilog(content, log_type: LogType="info"):
     if log_type == "error":
-        type_text = "\[\e[1;31m\]ERROR\[\e[m\]"
-        content_ = f"\[\e[1;31m\]{content}\[\e[m\]"
+        type_text = "\033[1;31mERROR\033[m"
+        content_ = f"\033[1;31m{content}\033[m"
     else:
-        type_text = "\[\e[1;36m\]INFO\[\e[m\]"
+        type_text = "\033[1;36mINFO\033[m"
         content_ = content
 
     now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_text = f"\[\e[1;90m\]{now_str}\[\e[m\] {type_text}\t\[\e[1;32m\]静山マシロ\[\e[m\] {content_}"
+    log_text = f"\033[1;90m{now_str}\033[m {type_text}\t\033[1;32m静山マシロ\033[m {content_}"
     print(log_text)
