a6f994f98bb679e044764d8840ebcc6c1b4ef3a0
mimaraka <nj.ot.niconico@gmail.com>
2023-09-15 05:50:13 +0900

    Update player.py

--- modules/music/player.py
+++ modules/music/player.py
@@ -181,7 +181,7 @@ class Player:
             self.__controller_msg = await self.__channel.send(**controller)
 
             if file := controller.get("file"):
-                os.remove(str(file.fp))
+                os.remove(file.fp.name)
 
 
     # 指定したトラックを強制的に再生
@@ -313,7 +313,7 @@ class Player:
             await self.__controller_msg.edit(**controller)
 
         if file := controller.get("file"):
-            os.remove(str(file.fp))
+            os.remove(file.fp.name)
 
 
     # コントローラーを再生成
@@ -324,7 +324,7 @@ class Player:
         self.__controller_msg = await channel.send(**controller)
 
         if file := controller.get("file"):
-            os.remove(str(file.fp))
+            os.remove(file.fp.name)
 
         if old_msg:
             try:
