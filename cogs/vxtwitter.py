9bcc4eca84acd0aca2ec3711aa6c6781486af6ac
mimaraka <106879397+mimaraka@users.noreply.github.com>
2023-10-16 11:41:05 +0900

    [Vxtwitter] クエリ削除機能を追加

--- cogs/vxtwitter.py
+++ cogs/vxtwitter.py
@@ -39,8 +39,9 @@ class Vxtwitter(discord.Cog):
 
     @discord.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if re.fullmatch(r"https?://(x|twitter).com/\w+/status/\d+", message.content):
-            new_url = re.sub(r"(x|twitter).com", r"vxtwitter.com", message.content)
+        if re.fullmatch(r"https?://(x|twitter).com/\w+/status/\d+(\?[\w=]*)?", message.content):
+            new_url = re.sub(r"(x|twitter).com", "vxtwitter.com", message.content)
+            new_url = re.sub(r"\?[\w=]*", "", new_url)
             result = f"**{message.author.display_name}** 先生が共有しました！ | [X]({new_url})"
             manage_messages = message.channel.permissions_for(message.guild.me)
             data = self.get_data()
