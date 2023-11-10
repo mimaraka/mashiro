3ad887f15a572ca80097d7d9763e5991dccc7034
mimaraka <nj.ot.niconico@gmail.com>
2023-11-10 18:40:44 +0900

    Update player.py

--- modules/music/player.py
+++ modules/music/player.py
@@ -393,7 +393,7 @@ class Player:
             else:
                 view = None
             seconds_sum = sum([track.duration.seconds if track.duration is not None else 0 for track in self.queue])
-            embed = MyEmbed(title=f"再生キュー ({len(self.queue)}曲 | {Duration(seconds_sum).text})", description=description)
+            embed = MyEmbed(title=f"再生キュー ({len(self.queue)}曲 | {Duration(seconds_sum)})", description=description)
         else:
             embed = MyEmbed(notif_type="inactive", title="再生キューは空です。")
             view = None
