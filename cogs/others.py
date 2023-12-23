eef7211be2fe3eca2452ab06c69e6dae74a3d2b5
mimaraka <nj.ot.niconico@gmail.com>
2023-12-23 14:01:50 +0900

    [Others] /helpコマンドのembedフィールド数超過を修正

--- cogs/others.py
+++ cogs/others.py
@@ -1,4 +1,5 @@
 import discord
+import math
 from modules.myembed import MyEmbed
 
 
@@ -15,12 +16,57 @@ class CogOthers(discord.Cog):
         await ctx.respond(embed=MyEmbed(title="お呼びですか、先生？", description=f"Ping値: `{ping}ms`"))
 
 
-    @discord.slash_command(name="help", description="何かお困りですか？")
-    async def command_help(self, ctx: discord.ApplicationContext):
-        embed = MyEmbed(title="コマンド一覧")
+    # コマンド一覧のメッセージを取得
+    async def get_help_msg(self, page: int, edit: bool=False):
+        commands = []
         for item in self.bot.walk_application_commands():
             if type(item) == discord.SlashCommand:
-                embed.add_field(name=f"/{item.name}", value=item.description, inline=False)
+                commands.append({
+                    "name": item.name,
+                    "description": item.description
+                })
+
+        n_pages = math.ceil(len(commands) / 10)
+
+        class ButtonPreviousPage(discord.ui.Button):
+            def __init__(btn_self, page: int):
+                btn_self.page: int = page
+                super().__init__(style=discord.enums.ButtonStyle.primary, disabled=page <= 1, emoji="⬅️")
+            
+            async def callback(btn_self, interaction: discord.Interaction):
+                await interaction.response.defer()
+                await interaction.followup.edit_message(interaction.message.id, **self.get_help_msg(page=btn_self.page - 1, edit=True))
+
+        class ButtonNextPage(discord.ui.Button):
+            def __init__(btn_self, page: int):
+                btn_self.page: int = page
+                super().__init__(style=discord.enums.ButtonStyle.primary, disabled=page >= n_pages, emoji="➡️")
+            
+            async def callback(btn_self, interaction: discord.Interaction):
+                await interaction.response.defer()
+                await interaction.followup.edit_message(interaction.message.id, **self.get_help_msg(page=btn_self.page + 1, edit=True))
+
+        embed = MyEmbed(title="コマンド一覧")
         view = discord.ui.View(timeout=None)
+        page = min(max(page, 1), n_pages)
+        start_index = (page - 1) * 10
+        for i in range(start_index, start_index + 10):
+            embed.add_field(name=f"/{commands[i]['name']}", value=commands[i]["description"])
+
+        if len(commands) > 10:
+            embed.description = f"**{page}** / {n_pages}ページ"
+            view.add_item(ButtonPreviousPage(page))
+            view.add_item(ButtonNextPage(page))
         view.add_item(discord.ui.Button(label='GitHubリポジトリ', url='https://github.com/mimaraka/mashiro'))
-        await ctx.respond(embed=embed, view=view, ephemeral=True)
+
+        result = {
+            "embed": embed,
+            "view": view
+        }
+        return result
+
+
+    @discord.slash_command(name="help", description="何かお困りですか？")
+    async def command_help(self, ctx: discord.ApplicationContext):
+        await ctx.defer(ephemeral=True)
+        await ctx.respond(**self.get_help_msg(page=1))
