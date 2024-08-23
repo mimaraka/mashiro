a0b154277849b1a37f8e7d7229946a1cbc9896e1
mimaraka <nj.ot.niconico@gmail.com>
2024-08-23 14:52:34 +0900

    キー名の修正

--- modules/common_embed.py
+++ modules/common_embed.py
@@ -5,7 +5,7 @@ from character_config import CHARACTER_TEXT
 EMBED_NOT_ADMINISTRATOR = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_author_not_admin'])
 EMBED_GUILD_ONLY = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_guild_only'])
 EMBED_BOT_NOT_PERMITTED = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_bot_not_permitted'])
-EMBED_USER_NOT_PERMITTED = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_user_not_permitted'])
+EMBED_USER_NOT_PERMITTED = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_author_not_permitted'])
 EMBED_BOT_NOT_CONNECTED = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_bot_not_connected'])
 EMBED_AUTHOR_NOT_CONNECTED = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_author_not_connected'])
 EMBED_BOT_ANOTHER_VC = MyEmbed(notif_type='error', description=CHARACTER_TEXT['error_bot_another_vc'])
